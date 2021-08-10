befcdad670804f4db22c499bdf4c50d5a4e3cd2f
Mike Francis <mike.francis@live.com>
2021-08-10 08:01:32 -0700

    initial commit

---
+++ TitleBar/README.MD
@@ -0,0 +1,24 @@
+# Sample: Title Bar Customization WinUI 3.0 Desktop
+
+## Description
+
+This sample was built using the [Windows App SDK ](https://docs.microsoft.com/en-us/windows/apps/windows-app-sdk/) version 0.8 and demonstrates the following:
+1) Override of Window title in title bar and the title shown with ALT+TAB
+
+2) Hiding of default title bar and replacement of title bar with one defined in MainWindow.xaml
+
+3) Customization of title bar background for foreground and disabled colors.
+
+# Contributing
+
+This project welcomes contributions and suggestions.  Most contributions require you to agree to a
+Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
+the rights to use your contribution. For details, visit https://cla.microsoft.com.
+
+When you submit a pull request, a CLA-bot will automatically determine whether you need to provide
+a CLA and decorate the PR appropriately (e.g., label, comment). Simply follow the instructions
+provided by the bot. You will only need to do this once across all repos using our CLA.
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
+For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
+contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
---
+++ TitleBar/TitleBar/TitleBar/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace TitleBar
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            SolidColorBrush transparent = new SolidColorBrush(Windows.UI.Color.FromArgb(0, 0, 0, 255)); // transparent (blue)
+            this.Resources["WindowCaptionBackground"] = transparent;
+            SolidColorBrush blue = new SolidColorBrush(Windows.UI.Color.FromArgb(120, 0, 0, 255)); // blue
+            this.Resources["WindowCaptionBackgroundDisabled"] = blue;
+            // https://docs.microsoft.com/en-us/windows/winui/api/microsoft.ui.xaml.window.settitlebar?view=winui-3.0
+
+            m_window = new MainWindow();
+            m_window.ExtendsContentIntoTitleBar = true;
+            m_window.Activate();
+        }
+
+        private Window m_window;
+    }
+}
